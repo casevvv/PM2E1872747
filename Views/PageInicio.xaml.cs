75ceecb0e2efb40bef8d00cc1de2aa4f08688ee6
Daniela Reyes <reyesdaniela498@gmail.com>
2024-02-17 17:26:53 -0600

    Imagen

--- Views/PageInicio.xaml.cs
+++ Views/PageInicio.xaml.cs
@@ -12,6 +12,8 @@ public partial class PageInicio : ContentPage
 {
 
     Controllers.RegistroController registro;
+    FileResult photo;
+
     public List<Pin> Pins { get; set; } = new List<Pin>();
 
     public PageInicio()
@@ -151,4 +153,21 @@ public partial class PageInicio : ContentPage
         await DisplayAlert("¡Hola!,", pinInfo.Address, "OK");
     }
 
-}
+
+    private async void btnFoto_Clicked(object sender, EventArgs e)
+
+    {
+
+        photo = await MediaPicker.CapturePhotoAsync();
+        if (photo != null)
+        {
+            string path = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+            using Stream sourcephoto = await photo.OpenReadAsync();
+            using FileStream StreamLocal = File.OpenWrite(path);
+
+            foto.Source = ImageSource.FromStream(() => photo.OpenReadAsync().Result);
+            await sourcephoto.CopyToAsync(StreamLocal);
+        }
+    }
+
+  }
